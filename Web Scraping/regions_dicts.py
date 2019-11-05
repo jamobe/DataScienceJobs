e7f827b45b03a1dffb35d24889226ea8d4ecb6eb
Janina Mothes <janinamothes@web.de>
2019-11-05 16:30:56 +0100

    region dictionaries

---
+++ Web Scraping/regions_dicts.py
@@ -0,0 +1,20 @@
+ GER_regions= {
+     'BW':'Baden-Wuerttemberg',
+     'BY':'Bayern',
+     'BE':'Berlin',
+     'BB':'Brandenburg',
+     'HB':'Bremen',
+     'HH':'Hamburg',
+     'HE':'Hessen',
+     'MV':'Mecklenburg-Vorpommern',
+     'NI':'Niedersachsen',
+     'NRW':'Nordrhein-Westfalen',
+     'RP':'Rheinland-Pfalz',
+     'SL':'Saarland',
+     'SN':'Sachsen',
+     'ST':'Sachsen-Anhalt',
+     'SH':'Schleswig-Holstein',
+     'TH':'Thueringen'
+ }
+
+
