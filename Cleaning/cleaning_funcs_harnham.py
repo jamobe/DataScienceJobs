3a71352e6677b65127830c109499591b1eac908f
LUNDR <lundrm@gmail.com>
2019-11-13 19:59:35 +0100

    clean-up of scraping and cleaning files for harnham and indeed

---
+++ Cleaning/cleaning_funcs_harnham.py
@@ -0,0 +1,86 @@
+#function to clean the location column in the Harnham data set
+
+
+def remove_duped_info(pandas_df_col,list_of_cols_to_compare):
+    a=pandas_df_col
+    for i in list_of_cols_to_compare:
+        for j in range(len(a)):     
+            l=a[j].lower()
+            l_2 =str(i[j]).lower()
+            a[j] = l.replace(l_2," ")
+    return a
+
+
+def clean_column(pandas_df_col):
+    import re
+
+    a=pandas_df_col
+    for i in range(len(a)):
+        rep = {"[": " ",
+               "]": " ",
+               "|": " ",
+               "''": " ",
+               "'": " ",
+               ",": " ",
+               "+": " ",
+               "/": " ",
+               "€": " ",
+               "£": " ",
+               "$": " ",
+               "benefits":" ",
+               "THE COMPANY": " ",
+               "THE ROLE": " ",
+               "THE BENEFITS": " ",
+               "HOW TO APPLY": " ",
+               "KEYWORDS": " ",
+               "YOUR SKILLS AND EXPERIENCE": " ",
+               "YOUR SKILLS AND EXPERTISE": " ",
+               "Please register your interest by sending your CV via the Apply link on this page":" ",
+               "BENEFITS":" ",
+               "CONTACT":" ",
+               "OVERVIEW":" ",
+               "SALARY":" ",
+               "For further details":" ",
+               "to enquire about other roles please contact":" ",
+               "Nick Mandella":" ",
+               "Harnham":" ",
+               "On a daily basis":" ",
+               "you will be:":" ",
+               "you will join:":" ",
+               "!" :" ",
+               "." : " ",
+               "0" :" ",
+              "1" : " ",
+              "2" : " ",
+              "3" :" ",
+              "4" : " ",
+              "5" : " ",
+              "6" : " ",
+              "7" :" ",
+              "8" : " ",
+              "9" : " "}
+        rep = dict((re.escape(k), v) for k, v in rep.items()) 
+        pattern = re.compile("|".join(rep.keys()))
+        if type(a[i])!="str":a[i]=str(a[i])
+        #a[i]=str(pandas_df_col[i])
+        a[i]=(pattern.sub(lambda m: rep[re.escape(m.group(0))],a[i]))
+    return a
+
+def create_split_salary_range(pandas_df_col):
+    import pandas as pd
+    a = pandas_df_col.str.split(" - ", n = 1, expand = True)[0]
+    b = pandas_df_col.str.split(" - ", n = 1, expand = True)[1]
+
+    for x in range(len(b)):
+        if pd.isnull(b[x]):
+            b[x]=a[x]
+    return a,b
+
+def clean_salary(pandas_df_col,currency_symbol):
+    import pandas as pd
+    try:
+    a = [pandas_df_col[x].split(currency_symbol,1)[1] for x in range(len(pandas_df_col))]
+    
+    a = [a[x].split("per",1)[0] for x in range(len(a))]
+    a = pd.to_numeric(a)
+    return a    
--- Web_Scraping/scraping_indeed_uk.py
+++ Web_Scraping/scraping_indeed_uk.py
@@ -1,58 +1,145 @@
-### code to scrape indeed uk website, and dump a pickle of the raw data into a file on the local drive
+### code to scrape indeed uk website and dump a pickle of the raw data into a file on the local drive
 
 
 import requests, bs4, time
 import pandas as pd
 import os
-import scraping_funcs_indeeduk as sf
 from itertools import cycle
 import traceback
 from datetime import date
 import pickle
 
-#Define variables
-searchTerm="machine learning"
-PATH = "C:/Users/lundr/DataScienceJobs/data/pickles/"
-
-#create empty data frame with column headers
-ads=pd.DataFrame(columns=['company','job_title','salary','location','description','date','full_description','other_deets','extraction_date'])
-
-# loop for scraping
-#proxy_pool = cycle(proxies)
-
-for i in range(0,1000,10):
-    #proxy = next(proxy_pool)
-    time.sleep(1) #ensuring at least 1 second between page grabs
-    url = "https://www.indeed.co.uk/jobs?q="+searchTerm+"&filter=0&l="+"&start="+str(i)
-    res = requests.get(url)      
-    soup = bs4.BeautifulSoup(res.content)
-    df=pd.DataFrame()
-    #print(sf.extract_company_from_result(soup))
-    df['company'] = sf.extract_company_from_result(soup)
-    df['job_title'] = sf.extract_job_title_from_result(soup)
-    df['salary'] = sf.extract_salary_from_result(soup)
-    df['location'] = sf.extract_location_from_result(soup)
-    df['description'] = sf.extract_description_from_result(soup)
-    df['date'] = sf.extract_date_from_result(soup)
-
-    sub_urls=sf.extract_links(soup)
-    urls_list =[]
-    text_list=[]
-    deets_list=[]
-    for j in sub_urls:
-        res_sub = requests.get(j)
-        soup_sub = bs4.BeautifulSoup(res_sub.content)
-        desc=sf.extract_full_desc(soup_sub)
-        text_list.append(desc)
-        other_deets=sf.extract_headlines_from_result(soup_sub)
-        deets_list.append(other_deets)
-        urls_list.append(sub_urls)  
-    df['full_description']=pd.DataFrame(text_list)
-    df['other_deets']=pd.DataFrame(deets_list)
-    df['extraction_date'] = date.today()
-    ads=ads.append(df, ignore_index=True)
-    print(i)
 
 
+def extract_job_title_from_result(soup): 
+    import pandas as pd
+    jobs = []
+    for div in soup.find_all(name="div", attrs={"class":"row"}):
+        try:
+            for a in div.find_all(name="a", attrs={"data-tn-element":"jobTitle"}):
+                jobs.append(a["title"])
+        except:
+                jobs.append("Nothing_found")
+    return pd.DataFrame(jobs)
 
-ads.to_pickle(PATH+"RAW_indeeduk_"+searchTerm+str(date.today()))  
+def extract_salary_from_result(soup): 
+    salaries = []
+    for div in soup.find_all(name="div", attrs={"class":"row"}):
+        try:
+            salaries.append(div.find(name="span",attrs={"class":"salaryText"}).text)
+        except:
+            salaries.append("Nothing_found")
+    return salaries
+
+def extract_location_from_result(soup):
+    locations = []
+    for div in soup.find_all(name="div", attrs={"class":"row"}):
+        try:
+            locations.append(div.find("span", attrs={"class": "location accessible-contrast-color-location"}).text)
+        except:
+            locations.append("Nothing_found")
+   
+    return locations
+
+def extract_description_from_result(soup):
+    description = []
+    for div in soup.find_all(name="div", attrs={"class":"row"}):
+        try:
+            description.append(div.find("div", attrs={"class": "summary"}).text)
+        except:
+            description.append("Nothing_found")
+   
+    return description
+
+def extract_date_from_result(soup): 
+    import pandas as pd
+    date = []
+    for div in soup.find_all(name="div", attrs={"class":"row"}):
+        try:
+            date.append(div.find("span", attrs={"class": "date"}).text)
+        except:
+            date.append("Nothing_found")
+    return date
+
+def extract_company_from_result(soup): 
+    company = []
+    for div in soup.find_all(name="div", attrs={"class":"row"}):
+        try:
+            company.append(div.find("span", attrs={"class": "company"}).text)
+        except:
+            company.append("Nothing_found")
+   
+    return company
+
+def extract_links(soup):
+    links =[]
+    for div in soup.find_all(name='a', attrs={'class':'jobtitle turnstileLink'}):
+        links.append('https://www.indeed.co.uk'+str(div['href']))
+    return links
+
+def extract_full_desc(soup):
+    text=[x.text for x in soup.find_all(name="div",attrs={"id":"jobDescriptionText"})]
+    return text
+
+
+def extract_headlines_from_result(soup): 
+    import pandas as pd
+    headlines =[]
+    try:
+        vals=[x.text for x in soup.find_all(name="span",attrs={"class":"jobsearch-JobMetadataHeader-iconLabel"})]
+        a ='_'.join(vals)
+        headlines.append(a)
+    except:
+        headlines.append("Nothing_found")
+    return headlines
+
+
+
+
+if __name__ == "__main__":
+    #Define variables
+    searchTerm="econometrics"
+    PATH = "data/indeed_raw_pickles/"
+
+    #create empty data frame with column headers
+    ads=pd.DataFrame(columns=['company','job_title','salary','location','description','date','full_description','other_deets','extraction_date'])
+
+    # loop for scraping
+    #proxy_pool = cycle(proxies)
+
+    for i in range(0,1000,10):
+        #proxy = next(proxy_pool)
+        time.sleep(1) #ensuring at least 1 second between page grabs
+        url = "https://www.indeed.co.uk/jobs?q="+searchTerm+"&filter=0&l="+"&start="+str(i)
+        res = requests.get(url)      
+        soup = bs4.BeautifulSoup(res.content, features="lxml")
+        df=pd.DataFrame()
+        #print(sf.extract_company_from_result(soup))
+        df['company'] = extract_company_from_result(soup)
+        df['job_title'] = extract_job_title_from_result(soup)
+        df['salary'] = extract_salary_from_result(soup)
+        df['location'] = extract_location_from_result(soup)
+        df['description'] = extract_description_from_result(soup)
+        df['date'] = extract_date_from_result(soup)
+
+        sub_urls=extract_links(soup)
+        urls_list =[]
+        text_list=[]
+        deets_list=[]
+        for j in sub_urls:
+            res_sub = requests.get(j)
+            soup_sub = bs4.BeautifulSoup(res_sub.content, features="lxml")
+            desc= extract_full_desc(soup_sub)
+            text_list.append(desc)
+            other_deets=extract_headlines_from_result(soup_sub)
+            deets_list.append(other_deets)
+            urls_list.append(sub_urls)  
+        df['full_description']=pd.DataFrame(text_list)
+        df['other_deets']=pd.DataFrame(deets_list)
+        df['extraction_date'] = date.today()
+        ads=ads.append(df, ignore_index=True)
+        print(i)
+
+
+
+    ads.to_pickle(PATH+"RAW_indeeduk_"+searchTerm+str(date.today())+".pkl")
