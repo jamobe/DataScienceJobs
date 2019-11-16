51f34bc327b24e3f43e1ca4d94d7f143550b1b82
Janina Mothes <janinamothes@web.de>
2019-11-16 16:48:45 +0100

    correct scraping functions

--- Cleaning/Data_cleaning.py
+++ Cleaning/Data_cleaning.py
@@ -90,29 +90,61 @@ def check_locations(string):
     return ','.join(location)
 
 
+def convert_euro(value, currency):
+    """
+
+    :param value:
+    :param currency:
+    :return:
+    """
+    if currency == '$':
+        euro = value * 0.9
+    elif currency == '£':
+        euro = value * 1.14
+    elif currency == '€':
+        euro = value
+    else:
+        euro = np.NAN
+    return euro
+
+
+def check_jobtype(string):
+    """
+    Extract jobtype from description
+    :param string:
+    :return: currency
+    """
+    job_type = ['permanent', 'unbefristet']
+    found = [i for i in string.lower() if i in job_type]
+    if found:
+        found = 'permanent'
+    else:
+        found = np.NaN
+    return found
+
 if __name__ == "__main__":
-    website = 'indeed_de' #374
-    #website = 'monster' #776
-    #website = 'indeed_us' #235
+    #website = 'indeed_de' #444  or # 13198 with Salary-NaNs
+    #website = 'monster' #1033 or #  1253 with Salary-NaNs
+    website = 'indeed_us' #521 or # 3130 with Salary-NaNs
 
     path = os.getcwd()
     parent_folder, current_folder = os.path.split(path)
-    csv_path = '/data/'+website+'_all.csv'
+    csv_path = '/data/' + website + '_all.csv'
 
     df = pd.read_csv(path + csv_path, sep='\t')
     length0 = df.shape[0]
     print('Uploaded: ' + csv_path + '\n')
     print('Containing ' + str(length0) + ' entries... \n')
-
+    print(df.columns)
     df = df.drop_duplicates(
         subset=['description', 'salary', 'location', 'jobtype', 'industry', 'education', 'career', 'ref_code', 'url',
                 'job_title', 'company'], keep='last', inplace=False)
     length1 = df.shape[0]
-    print('Removed '+ str(length0-length1) + ' duplicate entries: ' + str(length1) + '...\n')
+    print('Removed ' + str(length0-length1) + ' duplicate entries: ' + str(length1) + '...\n')
 
     df = df[df.astype(str)['description'] != '[]']
     length2 = df.shape[0]
-    print('Removed '+ str(length1-length2) +' empty job descriptions: ' + str(length2) + '...\n')
+    print('Removed ' + str(length1-length2) + ' empty job descriptions: ' + str(length2) + '...\n')
 
     df = df.reset_index(drop=True)
 
@@ -137,7 +169,7 @@ if __name__ == "__main__":
 
     # conversion german . to , only for indeed_de
     if website =='indeed_de':
-        #df.loc[df.currency == '€', 'salary'] = df.loc[df.currency == '€', 'salary'].str.replace('.', ',000.')
+        # df.loc[df.currency == '€', 'salary'] = df.loc[df.currency == '€', 'salary'].str.replace('.', ',000.')
         df['salary_extract'] = df['salary'].apply(extract_salary_after)
         df.loc[df.currency == '€', 'salary_extract'] = df.loc[df.currency == '€', 'salary_extract'].str.replace('.', ',')
     else:
@@ -149,16 +181,44 @@ if __name__ == "__main__":
     df = df.drop(columns='salary_extract', axis=1)
     df = df.dropna(subset=['salary'])
 
+    # fill low or high salary with high or low salary
+    no_high_salary = df['salary_high'].isnull() & df['salary_low'].isnull()
+    df.loc[no_high_salary, 'salary_high'] = df.loc[no_high_salary,'salary_low']
+    no_low_salary = df['salary_low'].isnull() & ~df['salary_high'].isnull()
+    df.loc[no_low_salary, 'salary_low'] = df.loc[no_low_salary, 'salary_high']
+
+    existing_salary_limits = ~df['salary_high'].isnull() & ~df['salary_low'].isnull()
+
+    # calculate salary average
+    df.loc[existing_salary_limits, 'salary_average'] = (df.loc[existing_salary_limits, 'salary_high'] +
+                                                        df.loc[existing_salary_limits, 'salary_low'])/2
+
+    # convert all to Euro
+    df['salary_low_euros'] = df.apply(lambda row: convert_euro(row.salary_low, row.currency), axis=1)
+    df['salary_high_euros'] = df.apply(lambda row: convert_euro(row.salary_high, row.currency), axis=1)
+    df['salary_average_euros'] = df.apply(lambda row: convert_euro(row.salary_average, row.currency), axis=1)
+
+    # determining salary type: yearly, monthly, hourly, daily
     names = defaultdict(list)
     names['yearly'] = ['year', 'annum', 'p.a', 'Jahr']
     names['montly'] = ['month', 'Monat']
     names['hourly'] = ['hour', 'Stunde']
     names['daily'] = ['day', 'Tag']
+    df['salary'].fillna('', inplace=True)
     for key, values in names.items():
         for synonyms in values:
-            df.loc[df['salary'].str.contains(synonyms), 'per'] = key
+            df.loc[df['salary'].str.contains(synonyms), 'salary_type'] = key
+    df['salary'].replace('', np.NaN, inplace=True)
     length3 = df.shape[0]
-    print('Removed '+ str(length2-length3) + ' NaN from Salary column: ' + str(length3) + '...\n')
+    print('Removed ' + str(length2-length3) + ' NaN from Salary column: ' + str(length3) + '...\n')
+
+    #df.replace(np.NaN, 'Not available', inplace=True)
+    #df.loc[df['jobtype'].str.contains('Permanent') == True, 'jobtype'] = 'permanent'
+    #df['jobtype'].replace('Not available', np.NaN, inplace=True)
+    #df.loc[df.jobtype == 'Not available', 'jobtype'] = df.loc[df.jobtype == 'Not available', 'description'].apply(check_jobtype)
+    #df.loc[df.loc[df.jobtype == 'Not available', 'salary_type'] == 'yearly', 'jobtype'] = 'permanent'
+    #df.loc[df['jobtype'].str.contains('Not available') != True, 'jobtype'] = np.NaN
+    #print(df['jobtype'].unique())
 
     # get location from description if not available
     mask4 = df.location.isnull()
@@ -203,7 +263,7 @@ if __name__ == "__main__":
         location_UK = pd.read_csv(path + '/data/locations_UK.csv')
         location_EU= pd.read_csv(path + '/data/locations.csv')
         location = location_UK.append(location_EU, ignore_index=True)
-        df2 = pd.merge(df, location, on='location', how='left')  # .drop('Unnamed: 0', axis=1)
+        df2 = pd.merge(df, location, on='location', how='left')
         df2 = df2.reset_index(drop=True)
     print('Cleaned location data...\n')
 
@@ -212,15 +272,16 @@ if __name__ == "__main__":
     df2.loc[df2.location.str.contains('Deutschland'), 'location'] = 'NaN'
     print('Identified region and country for each location...\n')
 
-    output = '/data/cleaned_'+ website + '.csv'
+    output = '/data/cleaned_' + website + '.csv'
 
-    cols = ['description', 'salary', 'location', 'jobtype', 'industry', 'education',
-            'career', 'ref_code', 'url', 'job_title', 'company', 'salary_low',
-            'salary_high', 'posted_date', 'currency', 'per', 'region', 'country']
+    cols = ['job_title', 'company', 'description', 'salary', 'salary_low', 'salary_high',
+            'salary_average', 'salary_low_euros', 'salary_high_euros', 'salary_average_euros',
+            'location', 'jobtype', 'posted_date', 'region', 'country','ref_code', 'url',
+            'currency', 'salary_type']
     df3 = df2[cols]
 
     length4 = df3.shape[0]
-    print('Save '+ str(length4) +' results in ' + output + '...\n')
+    print('Save ' + str(length4) + ' results in ' + output + '...\n')
     df3.to_csv(path + output, index=False)
     print('Done!')
 
