fee3ad522fd1de06709b0db76ec6fbc85aed3d0d
LUNDR <lundrm@gmail.com>
2019-12-07 13:15:12 +0100

    made scrape for al job terms into one big loop

--- Web_Scraping/scraping_indeed_uk.py
+++ Web_Scraping/scraping_indeed_uk.py
@@ -98,7 +98,7 @@ def extract_headlines_from_result(soup):
 
 if __name__ == "__main__":
     #Define variables
-    searchTerm="data+analyst"
+    
     PATH = "data/indeed_raw_pickles/"
 
     #create empty data frame with column headers
@@ -106,40 +106,42 @@ if __name__ == "__main__":
 
     # loop for scraping
     #proxy_pool = cycle(proxies)
+    jobs =  ['data+science','data+analyst','business+intelligence','data+engineer','AI Scientist','Machine+Learning+Engineer','database+developer','solutions+architect','insight+analytics','database+administrator','data+analytics','data+security','CRM+manager','data+manager','econometrics','statistics']
+    for searchTerm in jobs:
+        print(searchTerm)
+        for i in range(0,1000,10):
+            #proxy = next(proxy_pool)
+            time.sleep(1) #ensuring at least 1 second between page grabs
+            url = "https://www.indeed.co.uk/jobs?q="+searchTerm+"&sort=date&filter=0&l="+"&start="+str(i)
+            res = requests.get(url)      
+            soup = bs4.BeautifulSoup(res.content, features="lxml")
+            df=pd.DataFrame()
+            #print(sf.extract_company_from_result(soup))
+            df['company'] = extract_company_from_result(soup)
+            df['job_title'] = extract_job_title_from_result(soup)
+            df['salary'] = extract_salary_from_result(soup)
+            df['location'] = extract_location_from_result(soup)
+            df['description'] = extract_description_from_result(soup)
+            df['date'] = extract_date_from_result(soup)
 
-    for i in range(0,1000,10):
-        #proxy = next(proxy_pool)
-        time.sleep(1) #ensuring at least 1 second between page grabs
-        url = "https://www.indeed.co.uk/jobs?q="+searchTerm+"&sort=date&filter=0&l="+"&start="+str(i)
-        res = requests.get(url)      
-        soup = bs4.BeautifulSoup(res.content, features="lxml")
-        df=pd.DataFrame()
-        #print(sf.extract_company_from_result(soup))
-        df['company'] = extract_company_from_result(soup)
-        df['job_title'] = extract_job_title_from_result(soup)
-        df['salary'] = extract_salary_from_result(soup)
-        df['location'] = extract_location_from_result(soup)
-        df['description'] = extract_description_from_result(soup)
-        df['date'] = extract_date_from_result(soup)
-
-        sub_urls=extract_links(soup)
-        urls_list =[]
-        text_list=[]
-        deets_list=[]
-        for j in sub_urls:
-            res_sub = requests.get(j)
-            soup_sub = bs4.BeautifulSoup(res_sub.content, features="lxml")
-            desc= extract_full_desc(soup_sub)
-            text_list.append(desc)
-            other_deets=extract_headlines_from_result(soup_sub)
-            deets_list.append(other_deets)
-            urls_list.append(sub_urls)  
-        df['full_description']=pd.DataFrame(text_list)
-        df['other_deets']=pd.DataFrame(deets_list)
-        df['extraction_date'] = date.today()
-        ads=ads.append(df, ignore_index=True)
-        print(i)
+            sub_urls=extract_links(soup)
+            urls_list =[]
+            text_list=[]
+            deets_list=[]
+            for j in sub_urls:
+                res_sub = requests.get(j)
+                soup_sub = bs4.BeautifulSoup(res_sub.content, features="lxml")
+                desc= extract_full_desc(soup_sub)
+                text_list.append(desc)
+                other_deets=extract_headlines_from_result(soup_sub)
+                deets_list.append(other_deets)
+                urls_list.append(sub_urls)  
+            df['full_description']=pd.DataFrame(text_list)
+            df['other_deets']=pd.DataFrame(deets_list)
+            df['extraction_date'] = date.today()
+            ads=ads.append(df, ignore_index=True)
+            print(i)
 
 
 
-    ads.to_pickle(PATH+"RAW_indeeduk_"+searchTerm+str(date.today())+".pkl")
+        ads.to_pickle(PATH+"RAW_indeeduk_"+searchTerm+str(date.today())+".pkl")
